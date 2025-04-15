c1f47153a9cfa91603d7ca8ea817321438f5f98c
Dione Santos <dionedev@gmail.com>
2025-04-15 14:25:56 -0300

    feat: configure routes and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
+import 'package:todoapp/data/repositories/to_do_local_repository.dart';
+import 'package:todoapp/shared/app_colors.dart';
+import 'package:todoapp/ui/pages/todo/new_task_page.dart';
+import 'package:todoapp/ui/pages/todo/to_do_controller.dart';
+import 'package:todoapp/ui/pages/todo/todo_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR');
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ToDoController(
+        toDoLocalRepository: ToDoLocalRepository(),
+      ),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +29,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ToDo App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        scaffoldBackgroundColor: AppColors.backgroundLight,
       ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/todo',
+      routes: {
+        '/todo': (context) => TodoPage(),
+        '/new': (context) => NewTaskPage(),
+      },
     );
   }
 }
