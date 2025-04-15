5d6b9b4bcbf1600cc4ade8bc9a2b856988c9fb0b
Dione Santos <dionedev@gmail.com>
2025-04-15 14:17:34 -0300

    feat: add colors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryLight = Color(0xFF0AB6AB);
+  static const Color backgroundLight = Color(0xFFF1F5F9);
+  static const Color textPrimaryLight = Color(0xFF1B1B1D);
+  static const Color textSecondaryLight = Color(0xFF948E8E);
+  static const Color borderLight = Color(0xFFD2CFCF);
+  static const Color accentLight = Color(0xFFEC4040);
+  static const Color white = Color(0xFFFFFFFF);
+}
